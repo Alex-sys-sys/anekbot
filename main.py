905acc1078ef65472aaf928105c6d279a392a16d
Alex-sys-sys <94735806+Alex-sys-sys@users.noreply.github.com>
2022-04-24 15:08:13 +0300

    some shit written

---
+++ main.py
@@ -0,0 +1,99 @@
+import logging
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler
+from telegram import ReplyKeyboardMarkup
+import random
+from sql_funct import add_anek, get_anek_random, get_named_anek
+from datetime import datetime
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = '5394892147:AAFSEgwGahLiIgJsJUbv7MyS7qxPQ5WMfWE'
+reply_keyboard = [['/start', '/anekdot'],
+                  ['/get_anekdot_random', '/get_anekdot_name']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+
+def anek(update, context):
+    update.message.reply_text('Отправляй')
+    return 1
+
+
+def response(update, context):
+    add_anek(update.message.text)
+    spis = ['Прекрасно',
+            'Хммммммммммммммммммммммммммммммммммммммммммм....',
+            'Ну такое', 'Хороший анекдот', 'Действительно', 'Неплохо']
+    update.message.reply_text(random.choice(spis))
+    return ConversationHandler.END
+
+
+def start(update, context):
+    update.message.reply_text('Это бот с базой данных анекдотов - вы можете сами её пополнять, отправляя боту анекдоты.'
+                              'Вы конечно можете отправлять суда всякую хрень, но это будет неудобно и вам,'
+                              ' когда вы будете запрашивать случайные анекдоты. Так что соблюдаем приличие.',
+                              reply_markup=markup)
+
+
+def stop(update, context):
+    update.message.reply_text('Ну не хочешь, как хочешь')
+    return ConversationHandler.END
+
+
+def get_anek_rand(update, context):
+    update.message.reply_text(get_anek_random())
+
+
+def anek_theme(update, context):
+    update.message.reply_text('Введитe тему анекдота')
+    return 1
+
+
+def get_name_anek(update, context):
+    an = get_named_anek(update.message.text)
+    for i in an:
+        update.message.reply_text(i)
+    return ConversationHandler.END
+
+
+def text(update, context):
+    add_anek(update.message.text)
+    spis = ['Прекрасно',
+            'Хммммммммммммммммммммммммммммммммммммммммммм....',
+            'Ну такое', 'Хороший анекдот', 'Действительно', 'Неплохо']
+    update.message.reply_text(random.choice(spis))
+
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    conv_handler1 = ConversationHandler(
+        entry_points=[CommandHandler('anekdot', anek)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, response)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    conv_handler2 = ConversationHandler(
+        entry_points=[CommandHandler('get_anekdot_name', anek_theme)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, get_name_anek)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('get_anekdot_random', get_anek_rand))
+    text_handler = MessageHandler(Filters.text
+                                  & ~Filters.command, text)
+    dp.add_handler(conv_handler1)
+    dp.add_handler(conv_handler2)
+    dp.add_handler(text_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sql_funct.py
@@ -0,0 +1,30 @@
+import random
+import sqlite3
+from datetime import datetime
+
+
+def add_anek(text, user=None):
+    conn = sqlite3.connect('anek_base.db')
+    cur = conn.cursor()
+    timeformated = f'{datetime.now()}'
+    cur.execute(f"""INSERT INTO anekdots(anektext, time) VALUES('{text}', '{timeformated}')""")
+    conn.commit()
+    conn.close()
+
+
+def get_anek_random():
+    conn = sqlite3.connect('anek_base.db')
+    cur = conn.cursor()
+    aneki = cur.execute(f"""SELECT anektext FROM anekdots""").fetchall()
+    anek = random.choice(aneki)
+    conn.close()
+    return anek[0]
+
+
+def get_named_anek(theme):
+    conn = sqlite3.connect('anek_base.db')
+    cur = conn.cursor()
+    aneki = cur.execute(f"""SELECT anektext FROM anekdots""").fetchall()
+    anek_chosen = [i[0] for i in aneki if theme.lower() in i[0].lower()]
+    conn.close()
+    return anek_chosen
